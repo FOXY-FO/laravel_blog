b49148200ffbc6a34d6ca6970c6adeac6486e899
Vlad <foxyfo1703@gmail.com>
2020-05-14 00:51:59 +0300

    created relationships

--- app/Tag.php
+++ app/Tag.php
@@ -2,9 +2,29 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    use Sluggable;
+
+    public function posts()
+    {
+        return $this->belongsToMany(
+            Post::class,
+            'posts_tags',
+            'tag_id',
+            'post_id'
+        );
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
